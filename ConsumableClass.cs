71bf66f7e41705aed864f7afb13a1af1ad632882
hoangkienit <87848229+hoangkienit@users.noreply.github.com>
2023-11-18 23:26:31 +0700

    Add function

---
+++ ConsumableClass.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Consum", menuName = "Item/Consumable")]
+public class ConsumableClass : ItemClass
+{
+    [Header("Consumable")]
+    public int healthRecovery;
+
+    public override ItemClass GetItem() { return this; }
+    public override ToolClass GetTool() { return null; }
+    public override MiscClass GetMisc() { return null; }
+    public override ConsumableClass GetConsumable() { return this; }
+}
---
+++ SlotClass.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class SlotClass
+{
+    [SerializeField] private ItemClass item;
+    [SerializeField] private int quanity = 0;
+
+    public SlotClass()
+    {
+        item = null;
+        quanity = 0;
+    }
+
+    public SlotClass(ItemClass _item, int _quantity)
+    {
+        this.item = _item;
+        this.quanity = _quantity;
+    }
+
+    public ItemClass GetItem() { return this.item; }
+    public int GetQuantity() { return this.quanity; }
+    public void AddQuantity(int _quantity) { quanity = quanity + _quantity; }
+    public void SubQuantity(int _quantity) { quanity = quanity - _quantity; }
+
+    public void AddItem(ItemClass item, int quantity)
+    {
+        this.item = item;
+        this.quanity = quantity;
+    }
+
+    public void RemoveItem()
+    {
+        this.item = null;
+        this.quanity = 0;
+    }
+}
